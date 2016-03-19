3dfd20f60dd3d689737bf2de56d7529d3e59df46
Boris-Em <boris.emorine@gmail.com>
2016-03-19 11:26:08 -0700

    Add Xcode project.

---
+++ GobanSampleProject/GobanSampleProject/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  GobanSampleProject
+//
+//  Created by Bobo on 3/19/16.
+//  Copyright © 2016 Boris Emorine. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
