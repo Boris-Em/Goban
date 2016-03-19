b4fe49b300cacd507c9e5aed88cfd62bae1a902b
Boris-Em <boris.emorine@gmail.com>
2016-03-19 11:28:20 -0700

    Add GobanView file.

---
+++ Goban/GobanView.swift
@@ -0,0 +1,13 @@
+//
+//  GobanView.swift
+//  GobanSampleProject
+//
+//  Created by Bobo on 3/19/16.
+//  Copyright © 2016 Boris Emorine. All rights reserved.
+//
+
+import UIKit
+
+class GobanView: UIView {
+
+}
