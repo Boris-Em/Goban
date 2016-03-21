a9a605f92883785d42386037cf75370096b421d8
Boris-Em <boris.emorine@gmail.com>
2016-03-20 22:57:28 -0700

    Create basic viewController for GobanView.

--- GobanSampleProject/GobanSampleProject/ViewController.swift
+++ GobanSampleProject/GobanSampleProject/ViewController.swift
@@ -8,16 +8,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, GobanTouchProtocol {
 
+    @IBOutlet weak var gobanView: GobanView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        gobanView.gobanSize = GobanSize(width: 9, height: 9)
+        gobanView.gobanTouchDelegate = self
+        
+        gobanView.setStoneAtGobanPoint(GobanPoint(x: 2, y: 2), gobanStoneColor: .White, overwrite: true)
+        gobanView.setStoneAtGobanPoint(GobanPoint(x: 3, y: 3), gobanStoneColor: .White, overwrite: true)
+        gobanView.setStoneAtGobanPoint(GobanPoint(x: 3, y: 4), gobanStoneColor: .Black, overwrite: true)
+        gobanView.setStoneAtGobanPoint(GobanPoint(x: 3, y: 10), gobanStoneColor: .Black, overwrite: true)
+        gobanView.setStoneAtGobanPoint(GobanPoint(x: 9, y: 7), gobanStoneColor: .Black, overwrite: true)
+        gobanView.setStoneAtGobanPoint(GobanPoint(x: 11, y: 4), gobanStoneColor: .Black, overwrite: true)
+        gobanView.setStoneAtGobanPoint(GobanPoint(x: 9, y: 5), gobanStoneColor: .White, overwrite: true)
+        gobanView.setStoneAtGobanPoint(GobanPoint(x: 9, y: 4), gobanStoneColor: .White, overwrite: true)
+    }
+    
+    func didTouchGobanWithClosestGobanPoint(gobanView: GobanView, gobanPoint: GobanPoint) {
+        gobanView.setStoneAtGobanPoint(gobanPoint, gobanStoneColor: gobanView.lastSetStonePlayer == .Black ? .White : .Black, overwrite: false)
+    }
+    
+    @IBAction func didTapClearGobanButton(sender: AnyObject) {
+        gobanView.clearGoban()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 }
 
